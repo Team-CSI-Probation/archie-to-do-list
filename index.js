99a074e6d713767658ee9ab7b65cf65ce6811375
123archie <93096219+123archie@users.noreply.github.com>
2022-01-08 00:50:33 +0530

    fifth commit

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 
-const button1 = document.getElementsById("b1");
-const button2 = document.getElementsById("btn");
-const h=document.getElemetsById("header");
-const List=document.getElementsByClassName('des');
-const    
+let s = [],title = [], desc = [], dl = [];
 
-button1.addEventListener('click',()=>{
-    const ul=divList.querySelector('ul');
-    const li=
+let ct = document.getElementById("tInput").value;
 
+
+
+const button1 = document.getElementById("b1");
+const button2 = document.getElementById("btn");
+const h=document.getElementsById("header");
+const List=document.getElementByClassName('des');
+   
+
+button1.addEventListener("click",()=>{
+    const ul=divh.querySelector('List');
+    const li=document.createElement('li');
+    ul.appendChild(li);
+})
+
+function addTask(title, desc, dl,i){
+    title[i] = title;
+    desc[i] = desc;
+    dl[i] = dl;
+    s[i] = i;
 }
