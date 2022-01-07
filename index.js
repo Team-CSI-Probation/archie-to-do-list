93162f74dfbe99c10d22e61b8034bcce97b5dec8
123archie <93096219+123archie@users.noreply.github.com>
2022-01-07 15:03:35 +0530

    third commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+const button1 = document.getElementsById("b1");
+const button2 = document.getElementsById("btn");
+const h=document.getElemetsById("header");
+const 
+    
+
+tit=document.getElementsByClassName('title').value;
+des=document.getElementsByClassName('description').value;
+
+document.getElementById('b1').onclick(function(){
+    if(onclick=='True')
+    add(tit);
+})
