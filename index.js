0e176cac43128c170283e49bb795a9d65264f668
123archie <93096219+123archie@users.noreply.github.com>
2022-01-08 17:03:30 +0530

    sixth commit

--- index.js
+++ index.js
@@ -1,25 +1,4 @@
 
-let s = [],title = [], desc = [], dl = [];
-
-let ct = document.getElementById("tInput").value;
 
 
 
-const button1 = document.getElementById("b1");
-const button2 = document.getElementById("btn");
-const h=document.getElementsById("header");
-const List=document.getElementByClassName('des');
-   
-
-button1.addEventListener("click",()=>{
-    const ul=divh.querySelector('List');
-    const li=document.createElement('li');
-    ul.appendChild(li);
-})
-
-function addTask(title, desc, dl,i){
-    title[i] = title;
-    desc[i] = desc;
-    dl[i] = dl;
-    s[i] = i;
-}
