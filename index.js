095f95c84b6b3f07b16e9157d57d110bdcd22973
123archie <93096219+123archie@users.noreply.github.com>
2022-01-08 22:38:09 +0530

    seventh commit

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-
+const h=document.getElementById('header');
+const b1=document.getElementById('b1');
+const b2=document.getElementById('b2');
+const tit=document.getElementById('tInput');
+const de=document.getElementById('des');
+const b=document.getElementById("btn");
+const 
 
 
 
