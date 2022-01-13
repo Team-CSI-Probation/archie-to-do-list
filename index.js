910c011cb181506028d49d92d746a900862bd273
123archie <93096219+123archie@users.noreply.github.com>
2022-01-14 00:21:28 +0530

    Twelfth commit

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ var list=document.getElementsByTagName('li');
 var i;
 for(i=0;i<list.length;i++)
 {
-    
+   var span=document.createElement("SPAN");
+   var text=document.createTextNode("txt");
+   span.className="close";
+   span.appendChild(text);
+   list[i].appendChild(span);
+}
+
 }
